de0412a12f05b4b7489b92c53ce756805cbee509
funkenstein.sw@gmail.com <funkenstein.sw@gmail.com@99015736-f687-42c9-adb0-788dc10adda4>
2013-04-29 22:53:59 +0000

    Rearranging docs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mark3/source/trunk@18 99015736-f687-42c9-adb0-788dc10adda4

--- embedded/services/dcpu/dcpu.cpp
+++ embedded/services/dcpu/dcpu.cpp
@@ -15,7 +15,7 @@ See license.txt for more information
 	\file dcpu.cpp
 	\brief Portable DCPU-16 CPU emulator
 	
-	\section DCPU_1 DCPU-16 Emulator
+	\page DCPU_1 DCPU-16 Emulator
 
 	The DCPU-16 is the in-game CPU used in the upcoming game 0x10^c, from the
 	creators of the wildly successful Minecraft.  While the DCPU is supposed
