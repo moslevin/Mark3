6cedabc1e723f58fe4931a5e33f1092fb00195a8
funkenstein.sw@gmail.com <funkenstein.sw@gmail.com@99015736-f687-42c9-adb0-788dc10adda4>
2013-06-01 14:45:48 +0000

    Updating profiling results, docs, testing.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mark3/source/trunk@40 99015736-f687-42c9-adb0-788dc10adda4

--- embedded/kernel/public/profiling_results.h
+++ embedded/kernel/public/profiling_results.h
@@ -7,22 +7,22 @@ information for user comparison, as well as to ensure that regressions are not
 being introduced into the system.
 
 \section PROFILEATE Date Performed 
-Sun Jan 27 10:36:50 EST 2013
+Sat Jun  1 10:43:06 EDT 2013
 \section COMPVER Compiler Information 
 The kernel and test code used in these results were built using the following compiler:
 ./profile.sh: 55: ./profile.sh: /home/moslevin/atmel/bin/avr-gcc: not found
 \endcode
 \section PROFRES Profiling Results
-    - Semaphore Initialization: 2356994244 cycles (averaged over 82 iterations)
-    - Semaphore Post (uncontested): 148 cycles (averaged over 82 iterations)
-    - Semaphore Pend (uncontested): 68 cycles (averaged over 82 iterations)
-    - Semaphore Flyback Time (Contested Pend): 1668 cycles (averaged over 82 iterations)
-    - Mutex Init: 4190211955 cycles (averaged over 82 iterations)
-    - Mutex Claim: 204 cycles (averaged over 82 iterations)
-    - Mutex Release: 47 cycles (averaged over 82 iterations)
-    - Thread Initialize: 7897 cycles (averaged over 82 iterations)
-    - Thread Start: 798 cycles (averaged over 82 iterations)
-    - Context Switch: 171 cycles (averaged over 82 iterations)
-    - Thread Schedule: 70 cycles (averaged over 82 iterations)
+    - Semaphore Initialization: 7 cycles (averaged over 83 iterations)
+    - Semaphore Post (uncontested): 180 cycles (averaged over 83 iterations)
+    - Semaphore Pend (uncontested): 67 cycles (averaged over 83 iterations)
+    - Semaphore Flyback Time (Contested Pend): 1553 cycles (averaged over 83 iterations)
+    - Mutex Init: 0 cycles (averaged over 83 iterations)
+    - Mutex Claim: 143 cycles (averaged over 83 iterations)
+    - Mutex Release: 49 cycles (averaged over 83 iterations)
+    - Thread Initialize: 7800 cycles (averaged over 83 iterations)
+    - Thread Start: 803 cycles (averaged over 83 iterations)
+    - Context Switch: 198 cycles (averaged over 83 iterations)
+    - Thread Schedule: 47 cycles (averaged over 83 iterations)
     . 
 */
