7f5e917c1982eb2be513acd229d98e426d4b5e56
funkenstein.sw@gmail.com <funkenstein.sw@gmail.com@99015736-f687-42c9-adb0-788dc10adda4>
2013-08-18 12:59:51 +0000

    -Added more tests for eventflag -Completed logic for eventflag IPC/sync object
    git-svn-id: svn+ssh://svn.code.sf.net/p/mark3/source/trunk@78 99015736-f687-42c9-adb0-788dc10adda4

--- embedded/tests/unit/ut_eventflag/ut_eventflag.cpp
+++ embedded/tests/unit/ut_eventflag/ut_eventflag.cpp
@@ -34,6 +34,7 @@ K_UCHAR aucThreadStack2[160];
 EventFlag clFlagGroup;
 volatile K_UCHAR ucFlagCount = 0;
 
+//---------------------------------------------------------------------------
 void WaitOnFlag1Any(void *unused_)
 {
     clFlagGroup.Wait(0x0001, EVENT_FLAG_ANY);
@@ -42,6 +43,7 @@ void WaitOnFlag1Any(void *unused_)
     Scheduler::GetCurrentThread()->Exit();
 }
 
+//---------------------------------------------------------------------------
 void WaitOnMultiAny(void *unused_)
 {
     clFlagGroup.Wait(0x5555, EVENT_FLAG_ANY);
@@ -50,6 +52,7 @@ void WaitOnMultiAny(void *unused_)
     Scheduler::GetCurrentThread()->Exit();
 }
 
+//---------------------------------------------------------------------------
 void WaitOnMultiAll(void *unused_)
 {
     clFlagGroup.Wait(0x5555, EVENT_FLAG_ALL);
@@ -58,9 +61,182 @@ void WaitOnMultiAll(void *unused_)
     Scheduler::GetCurrentThread()->Exit();
 }
 
+//---------------------------------------------------------------------------
+void WaitOnAny(void *mask_)
+{
+    K_USHORT usMask = *((K_USHORT*)mask_);
+    while(1)
+    {
+        clFlagGroup.Wait(usMask, EVENT_FLAG_ANY);
+        ucFlagCount++;
+        clFlagGroup.Clear(usMask);
+    }
+}
+
+//---------------------------------------------------------------------------
+void WaitOnAll(void *mask_)
+{
+    K_USHORT usMask = *((K_USHORT*)mask_);
+    while(1)
+    {
+        clFlagGroup.Wait(usMask, EVENT_FLAG_ALL);
+        ucFlagCount++;
+        clFlagGroup.Clear(usMask);
+    }
+}
+
 //===========================================================================
 // Define Test Cases Here
 //===========================================================================
+TEST(ut_waitany)
+{
+    // Test - ensure that threads can block using the "waitany" mechanism, and
+    // only wake up when bits from its pattern are encountered.
+    K_USHORT i;
+    K_USHORT usMask = 0x8000;
+
+    clFlagGroup.Init();
+    ucFlagCount = 0;
+
+    clThread1.Init(aucThreadStack1, 160, 7, WaitOnAny, (void*)(&usMask));
+    clThread1.Start();
+
+    Thread::Sleep(100);
+
+    EXPECT_EQUALS(ucFlagCount, 0);
+
+    usMask = 0x0001;
+    while(usMask)
+    {
+        clFlagGroup.Set(usMask);
+        Thread::Sleep(100);
+
+        if (usMask != 0x8000)
+        {
+            EXPECT_EQUALS(ucFlagCount, 0);
+        }
+        else
+        {
+            EXPECT_EQUALS(ucFlagCount, 1);
+        }
+
+        usMask <<= 1;
+    }
+    clThread1.Exit();
+
+    // Okay, that was a single bit-flag test.  Now let's try using a multi-bit flag
+    // and verify that any matching pattern will cause a wakeup
+
+    clFlagGroup.Init();
+    ucFlagCount = 0;
+    usMask = 0xAAAA;
+
+    clThread1.Init(aucThreadStack1, 160, 7, WaitOnAny, (void*)(&usMask));
+    clThread1.Start();
+
+    Thread::Sleep(100);
+
+    EXPECT_EQUALS(ucFlagCount, 0);
+
+    // Test point - the flag set should kick the test thread on even-indexed
+    // counters indexes.
+    for (i = 0; i < 16; i++)
+    {
+        K_UCHAR ucLastFlagCount = ucFlagCount;
+
+        clFlagGroup.Set((K_USHORT)(1 << i));
+
+        Thread::Sleep(100);
+        if ((i & 1) == 0)
+        {
+            EXPECT_EQUALS(ucFlagCount, ucLastFlagCount);
+        }
+        else
+        {
+            EXPECT_EQUALS(ucFlagCount, ucLastFlagCount+1);
+        }
+    }
+
+    clThread1.Exit();
+}
+TEST_END
+
+
+//===========================================================================
+TEST(ut_waitall)
+{
+    // Test - ensure that threads can block using the "waitany" mechanism, and
+    // only wake up when bits from its pattern are encountered.
+    K_USHORT i;
+    K_USHORT usMask = 0x8000;
+
+    clFlagGroup.Init();
+    ucFlagCount = 0;
+
+    clThread1.Init(aucThreadStack1, 160, 7, WaitOnAll, (void*)(&usMask));
+    clThread1.Start();
+
+    Thread::Sleep(100);
+
+    EXPECT_EQUALS(ucFlagCount, 0);
+
+    usMask = 0x0001;
+    while(usMask)
+    {
+        clFlagGroup.Set(usMask);
+        Thread::Sleep(100);
+
+        if (usMask != 0x8000)
+        {
+            EXPECT_EQUALS(ucFlagCount, 0);
+        }
+        else
+        {
+            EXPECT_EQUALS(ucFlagCount, 1);
+        }
+
+        usMask <<= 1;
+    }
+    clThread1.Exit();
+
+    // Okay, that was a single bit-flag test.  Now let's try using a multi-bit flag
+    // and verify that any matching pattern will cause a wakeup
+
+    clFlagGroup.Init();
+    ucFlagCount = 0;
+    usMask = 0xAAAA;
+
+    clThread1.Init(aucThreadStack1, 160, 7, WaitOnAll, (void*)(&usMask));
+    clThread1.Start();
+
+    Thread::Sleep(100);
+
+    EXPECT_EQUALS(ucFlagCount, 0);
+
+    // Test point - the flag set should kick the test thread on even-indexed
+    // counters indexes.
+    for (i = 0; i < 16; i++)
+    {
+        K_UCHAR ucLastFlagCount = ucFlagCount;
+
+        clFlagGroup.Set((K_USHORT)(1 << i));
+
+        Thread::Sleep(100);
+        if (i != 15)
+        {
+            EXPECT_EQUALS(ucFlagCount, ucLastFlagCount);
+        }
+        else
+        {
+            EXPECT_EQUALS(ucFlagCount, ucLastFlagCount+1);
+        }
+    }
+
+    clThread1.Exit();
+}
+TEST_END
+
+//---------------------------------------------------------------------------
 TEST(ut_flag_multiwait)
 {
 
@@ -72,6 +248,8 @@ TEST(ut_flag_multiwait)
     // Test point - 2 threads blocking on an event flag, bit 1.  Wait on these
     // threads until this thread sets bit 0x0001.  When that bit is set, the
     // threads should wake up, incrementing the "ucFlagCount" variable.
+    ucFlagCount = 0;
+    clFlagGroup.Clear(0xFFFF);
 
     clThread1.Init(aucThreadStack1, 160, 7, WaitOnFlag1Any, 0);
     clThread2.Init(aucThreadStack2, 160, 7, WaitOnFlag1Any, 0);
@@ -95,7 +273,6 @@ TEST(ut_flag_multiwait)
     // Test point - 2 threads blocking on an event flag, bits 0x5555.  Block
     // on these threads, and verify that only bits in the pattern will cause
     // the threads to awaken
-
     clThread1.Init(aucThreadStack1, 160, 7, WaitOnMultiAny, 0);
     clThread2.Init(aucThreadStack2, 160, 7, WaitOnMultiAny, 0);
 
@@ -119,6 +296,7 @@ TEST(ut_flag_multiwait)
     ucFlagCount = 0;
     clFlagGroup.Clear(0xFFFF);
 
+
     clThread1.Init(aucThreadStack1, 160, 7, WaitOnMultiAny, 0);
     clThread2.Init(aucThreadStack2, 160, 7, WaitOnMultiAny, 0);
 
@@ -138,7 +316,6 @@ TEST(ut_flag_multiwait)
     Thread::Sleep(100);
 
     EXPECT_EQUALS(ucFlagCount, 2);
-
     // Test point - same thing as above, but with the "ALL" flags set.
 
     ucFlagCount = 0;
@@ -194,7 +371,6 @@ TEST(ut_flag_multiwait)
     Thread::Sleep(100);
 
     EXPECT_EQUALS(ucFlagCount, 2);
-
 }
 TEST_END
 
@@ -202,5 +378,7 @@ TEST_END
 // Test Whitelist Goes Here
 //===========================================================================
 TEST_CASE_START
+  TEST_CASE(ut_waitany),
+  TEST_CASE(ut_waitall),
   TEST_CASE(ut_flag_multiwait),
 TEST_CASE_END
