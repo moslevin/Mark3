8f1c22690a4e7aa79765ad78476742f4d40ccadb
funkenstein.sw@gmail.com <funkenstein.sw@gmail.com@99015736-f687-42c9-adb0-788dc10adda4>
2014-01-02 15:52:19 +0000

    Updated st7753 example with a ball-and-paddle game example.

--- trunk/embedded/examples/avr/st7735/st7735_test.cpp
+++ trunk/embedded/examples/avr/st7735/st7735_test.cpp
@@ -29,6 +29,8 @@ See license.txt for more information
 #include "draw.h"
 #include "colorspace.h"
 #include "font.h"
+#include "timerlist.h"
+#include "ksemaphore.h"
 #include <avr/io.h>
 #include <avr/sleep.h>
 
@@ -42,6 +44,9 @@ static Thread AppThread;            //!< Main "application" thread
 static Thread IdleThread;            //!< Idle thread - runs when app can't
 
 static GraphicsST7735 clTFT;         //!< TFT Display driver
+
+static Semaphore clSem;
+static Timer clTimer;
 
 //---------------------------------------------------------------------------
 #define STACK_SIZE_APP        (320)    //!< Size of the main app's stack
@@ -79,15 +84,22 @@ int main(void)
 }
 
 //---------------------------------------------------------------------------
+void Timer_Callback(Thread *pclOwner_, void *unused_)
+{
+    clSem.Post();
+}
+
+//---------------------------------------------------------------------------
 void AppEntry(void)
 {        
     clTFT.SetName("/dev/display");     //!< Add the display driver
     clTFT.Init();
     clTFT.Open();
 
-    // Dummy code - just write hello world forever...
+    // Clear the screen
     clTFT.ClearScreen();
 
+    // Demo 1 - Write some text to the display
     DrawText_t stText;
 
     stText.usLeft = 8;
@@ -103,31 +115,133 @@ void AppEntry(void)
     stText.pstFont = &fntTahoma_10_False_False_False_;
     stText.pcString = "Goodbye World!";
     clTFT.Text(&stText);
-    Thread::Sleep(10);
+    Thread::Sleep(2000);
 
-    K_USHORT usXOff = 0;
-    K_USHORT usYOff = 0;
+    // Demo 2 - Draw an animated ball-and-paddle
+    clSem.Init(0, 1);
+    clTimer.Init();
+
+    // Create a timer that expires every 16ms (approx 60Hz).  We use this to time our
+    // updates to the display
+    clTimer.Start( true, 16, 0, Timer_Callback,0 );
+
+    // Variables used in the demo,
+    K_USHORT usBallX = 0;
+    K_USHORT usBallY = 0;
+
+    bool bBallXDir = false;
+    bool bBallYDir = false;
+
+    // Objects used to draw/undraw the ball and paddle
+    DrawRectangle_t stBall;
+    DrawRectangle_t stPaddle;
+
+    // Seed the ball/paddle original position.
+    stBall.bFill = true;
+    stBall.usLeft = usBallX;
+    stBall.usTop = usBallY;
+    stBall.usRight = usBallX+4;
+    stBall.usBottom = usBallY+4;
+    stBall.uLineColor = COLOR_BLUE;
+    stBall.uFillColor = COLOR_BLUE;
+
+    stPaddle.bFill = true;
+
+    if (usBallX < 12)
+    {
+        stPaddle.usLeft = 0;
+    }
+    else if (usBallX >= 116)
+    {
+        stPaddle.usLeft = 104;
+    }
+    else
+    {
+        stPaddle.usLeft = usBallX - 12;
+    }
+    stPaddle.usRight = stPaddle.usLeft + 24;
+    stPaddle.uLineColor = COLOR_WHITE;
+    stPaddle.uFillColor = COLOR_WHITE;
+    stPaddle.usTop = 152;
+    stPaddle.usBottom = 155;
+
+    // Draw the initial ball/paddle image
+    clTFT.Rectangle(&stBall);
+    clTFT.Rectangle(&stPaddle);
 
     while(1)
     {
+        // Wait for our 60Hz timer to post the semaphore.
+        clSem.Pend();
 
-        for (usXOff = 0; usXOff <= 120; usXOff+= 8)
+        // "Undraw" the previous ball, redraw it as quickly as possible at new location
+        stBall.uFillColor = COLOR_BLACK;
+        stBall.uLineColor = COLOR_BLACK;
+        clTFT.Rectangle(&stBall);
+
+        if (bBallXDir)
         {
-            for (usYOff = 0; usYOff <= 152; usYOff += 8)
+            usBallX -= 2;
+            if (usBallX == 0)
             {
-                DrawRectangle_t stRect;
-
-                stRect.bFill = true;
-                stRect.usLeft = usXOff;
-                stRect.usTop = usYOff;
-                stRect.usRight = usXOff+7;
-                stRect.usBottom = usYOff+7;
-                stRect.uLineColor = COLOR_BLUE;
-                stRect.uFillColor = COLOR_BLUE;
-                clTFT.Rectangle(&stRect);
+                bBallXDir = false;
             }
         }
-        clTFT.ClearScreen();
+        else
+        {
+            usBallX += 2;
+            if (usBallX >= 120)
+            {
+                bBallXDir = true;
+            }
+        }
+
+        if (bBallYDir)
+        {
+            usBallY -= 2;
+            if (usBallY == 0)
+            {
+                bBallYDir = false;
+            }
+        }
+        else
+        {
+            usBallY += 2;
+            if (usBallY >= 148)
+            {
+                bBallYDir = true;
+            }
+        }
+        stBall.usLeft = usBallX;
+        stBall.usTop = usBallY;
+        stBall.usRight = usBallX+4;
+        stBall.usBottom = usBallY+4;
+        stBall.uLineColor = COLOR_BLUE;
+        stBall.uFillColor = COLOR_BLUE;
+
+        clTFT.Rectangle(&stBall);
+
+        // Undraw previous paddle, redraw as quickly as possible at new location
+        stPaddle.uLineColor = COLOR_BLACK;
+        stPaddle.uFillColor = COLOR_BLACK;
+        clTFT.Rectangle(&stPaddle);
+
+        if (usBallX < 12)
+        {
+            stPaddle.usLeft = 0;
+        }
+        else if (usBallX >= 116)
+        {
+            stPaddle.usLeft = 104;
+        }
+        else
+        {
+            stPaddle.usLeft = usBallX - 12;
+        }
+        stPaddle.usRight = stPaddle.usLeft + 24;
+        stPaddle.uLineColor = COLOR_WHITE;
+        stPaddle.uFillColor = COLOR_WHITE;
+        clTFT.Rectangle(&stPaddle);
     }
 }
 
