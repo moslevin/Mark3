249735c59e520e688e04f349038a77d7aea7f09b
m0slevin <m0slevin@99015736-f687-42c9-adb0-788dc10adda4>
2014-10-16 02:48:04 +0000

    -Added flAVR based simulator tests and profiling, which now work equivalently well (better in some cases) to simavr.

--- trunk/embedded/scripts/sim_test.py
+++ trunk/embedded/scripts/sim_test.py
@@ -1,20 +1,19 @@
 ### Quick script to automate the execution of all unit tests
-### By running the tests on the simavr simulator
+### By running the tests on the flavr simulator
 import pexpect
 
 # Set the environment variables for the test platform
 platform  = "avr"
-cpu       = "atmega644"
+cpu       = "atmega328p"
 toolchain = "gcc"
 stage	= "./stage"
-
 # List of unit tests to run
 test_list = ["ut_logic", "ut_thread", "ut_semaphore", "ut_mutex", "ut_eventflag", "ut_heap", "ut_message", "ut_timers"]
 
 # Run each test in succession
 for test in test_list:
 	# Build the commandline used to run the tests
-	test_cmd = "simavr -f 16000000 -m %s %s/app/%s/%s/%s/%s.elf" % (cpu, stage, platform, cpu, toolchain, test)
+	test_cmd = "flavr --silent --hexfile %s/app/%s/%s/%s/%s.hex" % (stage, platform, cpu, toolchain, test)
 	child = pexpect.spawn( test_cmd )
 	print "--[Running Test: %s]--" % test 
 	index = child.expect (["--DONE--","(FAIL)", pexpect.EOF, pexpect.TIMEOUT], timeout=240)
