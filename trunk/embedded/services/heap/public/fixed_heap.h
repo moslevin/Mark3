c640c4392e1dc898d960090de4b240123a45b2c9
m0slevin <m0slevin@99015736-f687-42c9-adb0-788dc10adda4>
2015-09-26 17:42:24 +0000

    Comment Cleanup, continued.

--- trunk/embedded/services/heap/public/fixed_heap.h
+++ trunk/embedded/services/heap/public/fixed_heap.h
@@ -10,12 +10,12 @@
 
 Copyright (c) 2012-2015 Funkenstein Software Consulting, all rights reserved.
 See license.txt for more information
-===========================================================================*/
+=========================================================================== */
 /*!
     \file "fixed_heap.h"
     
     \brief Fixed-block-size heaps
-*/
+ */
 #ifndef __FIXED_HEAP_H__
 #define __FIXED_HEAP_H__
 
@@ -24,53 +24,53 @@ See license.txt for more information
 
 //---------------------------------------------------------------------------
 /*!
-    Single-block-size heap
-*/
+ *  Single-block-size heap
+ */
 class BlockHeap
 {
 public:
     /*!
-    
-        \fn void *Create( void *pvHeap_, uint16_t u16Size_, uint16_t u16BlockSize_ )
-        
-        Create a single list heap in the blob of memory provided, with the 
-        selected heap size, and the selected number of blocks.  Will create
-        as many blocks as will fit in the u16Size_ parameter
-        
-        \param pvHeap_ Pointer to the heap data to initialize
-        \param u16Size_ Size of the heap range in bytes
-        \param u16BlockSize_ Size of each heap block in bytes
-        
-        \return Pointer to the next heap element to initialize
-    */
+     *
+     *  \fn void *Create( void *pvHeap_, uint16_t u16Size_, uint16_t u16BlockSize_ )
+     *  
+     *  Create a single list heap in the blob of memory provided, with the 
+     *  selected heap size, and the selected number of blocks.  Will create
+     *  as many blocks as will fit in the u16Size_ parameter
+     *  
+     *  \param pvHeap_ Pointer to the heap data to initialize
+     *  \param u16Size_ Size of the heap range in bytes
+     *  \param u16BlockSize_ Size of each heap block in bytes
+     *  
+     *  \return Pointer to the next heap element to initialize
+     */
     void *Create( void *pvHeap_, uint16_t u16Size_, uint16_t u16BlockSize_ );
     
     /*! 
-        \fn void *Alloc()
-        
-        Allocate a block of memory from this heap
-        
-        \return pointer to a block of memory, or 0 on failure
-    */
+     *  \fn void *Alloc()
+     *  
+     *  Allocate a block of memory from this heap
+     *  
+     *  \return pointer to a block of memory, or 0 on failure
+     */
     void *Alloc();
     
     /*!
-        \fn void Free( void* pvData_ )
-        
-        Free a previously allocated block of memory
-        
-        \param pvData_ Pointer to a block of data previously allocated off
-               the heap.
-    */
+     *  \fn void Free( void* pvData_ )
+     *  
+     *  Free a previously allocated block of memory
+     *  
+     *  \param pvData_ Pointer to a block of data previously allocated off
+     *         the heap.
+     */
     void Free( void* pvData_ );
     
     /*!
-        \fn bool IsFree() 
-        
-        Returns the state of a heap - whether or not it has free elements.
-        
-        \return true if the heap is not full, false if the heap is full
-    */
+     *  \fn bool IsFree() 
+     *  
+     *  Returns the state of a heap - whether or not it has free elements.
+     *  
+     *  \return true if the heap is not full, false if the heap is full
+     */
     bool IsFree() { return m_u16BlocksFree != 0; }
     
 protected:
@@ -86,7 +86,7 @@ class FixedHeap;
 //---------------------------------------------------------------------------
 /*!
     Heap configuration object
-*/
+ */
 class HeapConfig
 {
 public:
@@ -100,51 +100,51 @@ protected:
 //---------------------------------------------------------------------------
 /*!
     Fixed-size-block heap allocator with multiple block sizes.
-*/
+ */
 class FixedHeap
 {
 public:
     /*!
-        \fn void Create( void *pvHeap_, HeapConfig *pclHeapConfig_ )
-    
-        Creates a heap in a provided blob of memory with lists of fixed-size
-        blocks configured based on the associated configuration data.  A
-        heap must be created before it can be allocated/freed.
-    
-        \param pvHeap_ Pointer to the data blob that will contain the heap
-        \param pclHeapConfig_ Pointer to the array of config objects that
-               define how the heap is laid out in memory, and how many
-               blocks of what size are included.  The objects in the 
-               array must be initialized, starting from smallest block-size
-               to largest, with the final entry in the table have a 
-               0-block size, indicating end-of-configuration.
-    */
+     *  \fn void Create( void *pvHeap_, HeapConfig *pclHeapConfig_ )
+     *
+     *  Creates a heap in a provided blob of memory with lists of fixed-size
+     *  blocks configured based on the associated configuration data.  A
+     *  heap must be created before it can be allocated/freed.
+     *
+     *  \param pvHeap_ Pointer to the data blob that will contain the heap
+     *  \param pclHeapConfig_ Pointer to the array of config objects that
+     *         define how the heap is laid out in memory, and how many
+     *         blocks of what size are included.  The objects in the 
+     *         array must be initialized, starting from smallest block-size
+     *         to largest, with the final entry in the table have a 
+     *         0-block size, indicating end-of-configuration.
+     */
     void Create( void *pvHeap_, HeapConfig *pclHeapConfig_ );
 
     /*!
-        \fn void *Alloc( uint16_t u16Size_ )
-        
-        Allocate a blob of memory from the heap.  If no appropriately-sized
-        data block is available, will return NULL.  Note, this API is thread-
-        safe, and interrupt safe.
-                
-        \param u16Size_ Size (in bytes) to allocate from the heap
-        
-        \return Pointer to a block of data allocated, or 0 on error.
-    */
+     *  \fn void *Alloc( uint16_t u16Size_ )
+     *  
+     *  Allocate a blob of memory from the heap.  If no appropriately-sized
+     *  data block is available, will return NULL.  Note, this API is thread-
+     *  safe, and interrupt safe.
+     *          
+     *  \param u16Size_ Size (in bytes) to allocate from the heap
+     *  
+     *  \return Pointer to a block of data allocated, or 0 on error.
+     */
     void *Alloc( uint16_t u16Size_ );
     
     /*!
-        \fn void Free( void *pvNode_ )
-        
-        Free a previously-allocated block of memory to the heap it was 
-        originally allocated from.  This must point to the block of
-        memory at its originally-returned pointer, and not an address 
-        within an allocated blob (as supported by some allocators).
-        
-        \param pvNode_ Pointer to the previously-allocated block of memory
-        
-    */
+     *  \fn void Free( void *pvNode_ )
+     *  
+     *  Free a previously-allocated block of memory to the heap it was 
+     *  originally allocated from.  This must point to the block of
+     *  memory at its originally-returned pointer, and not an address 
+     *  within an allocated blob (as supported by some allocators).
+     *  
+     *  \param pvNode_ Pointer to the previously-allocated block of memory
+     *  
+     */
     static void Free( void *pvNode_ );
     
 private:
--- trunk/embedded/services/memutil/public/memutil.h
+++ trunk/embedded/services/memutil/public/memutil.h
@@ -10,13 +10,13 @@
 
 Copyright (c) 2012-2015 Funkenstein Software Consulting, all rights reserved.
 See license.txt for more information
-===========================================================================*/
+=========================================================================== */
 /*!
 
     \file   memutil.h
 
     \brief  Utility class containing memory, string, and conversion routines.
-*/
+ */
 
 #ifndef __MEMUTIL_H__
 #define __MEMUTIL_H__
@@ -28,7 +28,7 @@ See license.txt for more information
 //---------------------------------------------------------------------------
 /*!
     \brief Token descriptor struct format
-*/
+ */
 typedef struct
 {
     const char *pcToken;  //!< Pointer to the beginning of the token string
@@ -43,7 +43,7 @@ typedef struct
     functions, without relying on an external standard library implementation
     which might not be available on some toolchains, may be closed source,
     or may not be thread-safe.
-*/
+ */
 class MemUtil
 {
 
@@ -51,149 +51,149 @@ public:
 
     //-----------------------------------------------------------------------
     /*!
-        Convert an 8-bit unsigned binary value as a hexadecimal string.
-
-        \fn static void DecimalToHex( uint8_t u8Data_, char *szText_ )
-
-        \param u8Data_ Value to convert into a string
-        \param szText_ Destination string buffer (3 bytes minimum)
-    */
+     *  Convert an 8-bit unsigned binary value as a hexadecimal string.
+     *
+     *  \fn static void DecimalToHex( uint8_t u8Data_, char *szText_ )
+     *
+     *  \param u8Data_ Value to convert into a string
+     *  \param szText_ Destination string buffer (3 bytes minimum)
+     */
     static void DecimalToHex( uint8_t u8Data_, char *szText_ );
     static void DecimalToHex( uint16_t u16Data_, char *szText_ );
     static void DecimalToHex( uint32_t u32Data_, char *szText_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Convert an 8-bit unsigned binary value as a decimal string.
-
-        \fn static void DecimalToString( uint8_t u8Data_, char *szText_ )
-
-        \param u8Data_ Value to convert into a string
-        \param szText_ Destination string buffer (4 bytes minimum)
-    */
+     *  Convert an 8-bit unsigned binary value as a decimal string.
+     *
+     *  \fn static void DecimalToString( uint8_t u8Data_, char *szText_ )
+     *
+     *  \param u8Data_ Value to convert into a string
+     *  \param szText_ Destination string buffer (4 bytes minimum)
+     */
     static void DecimalToString( uint8_t u8Data_, char *szText_ );
     static void DecimalToString( uint16_t u16Data_, char *szText_ );
     static void DecimalToString( uint32_t u32Data_, char *szText_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Compute the 8-bit addative checksum of a memory buffer.
-
-        \fn static uint16_t Checksum8( const void *pvSrc_, uint16_t u16Len_ )
-
-        \param pvSrc_ Memory buffer to compute a 8-bit checksum of.
-        \param u16Len_ Length of the buffer in bytes.
-        \return 8-bit checksum of the memory block.
-    */
+     *  Compute the 8-bit addative checksum of a memory buffer.
+     *
+     *  \fn static uint16_t Checksum8( const void *pvSrc_, uint16_t u16Len_ )
+     *
+     *  \param pvSrc_ Memory buffer to compute a 8-bit checksum of.
+     *  \param u16Len_ Length of the buffer in bytes.
+     *  \return 8-bit checksum of the memory block.
+     */
     static uint8_t  Checksum8( const void *pvSrc_, uint16_t u16Len_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Compute the 16-bit addative checksum of a memory buffer.
-
-        \fn static uint16_t Checksum16( const void *pvSrc_, uint16_t u16Len_ )
-
-        \param pvSrc_ Memory buffer to compute a 16-bit checksum of.
-        \param u16Len_ Length of the buffer in bytes.
-        \return 16-bit checksum of the memory block.
-    */
+     *  Compute the 16-bit addative checksum of a memory buffer.
+     *
+     *  \fn static uint16_t Checksum16( const void *pvSrc_, uint16_t u16Len_ )
+     *
+     *  \param pvSrc_ Memory buffer to compute a 16-bit checksum of.
+     *  \param u16Len_ Length of the buffer in bytes.
+     *  \return 16-bit checksum of the memory block.
+     */
     static uint16_t Checksum16( const void *pvSrc_, uint16_t u16Len_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Compute the length of a string in bytes.
-
-        \fn static uint16_t StringLength( const char *szStr_ )
-
-        \param szStr_ Pointer to the zero-terminated string to calculate the length of
-        \return length of the string (in bytes), not including the 0-terminator.
-
-    */
+     *  Compute the length of a string in bytes.
+     *
+     *  \fn static uint16_t StringLength( const char *szStr_ )
+     *
+     *  \param szStr_ Pointer to the zero-terminated string to calculate the length of
+     *  \return length of the string (in bytes), not including the 0-terminator.
+     *
+     */
     static uint16_t StringLength( const char *szStr_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Compare the contents of two zero-terminated string buffers to eachother.
-
-        \fn static bool CompareStrings( const char *szStr1_, const char *szStr2_ )
-
-        \param szStr1_ First string to compare
-        \param szStr2_ Second string to compare
-        \return true if strings match, false otherwise.
-    */
+     *  Compare the contents of two zero-terminated string buffers to eachother.
+     *
+     *  \fn static bool CompareStrings( const char *szStr1_, const char *szStr2_ )
+     *
+     *  \param szStr1_ First string to compare
+     *  \param szStr2_ Second string to compare
+     *  \return true if strings match, false otherwise.
+     */
     static bool CompareStrings( const char *szStr1_, const char *szStr2_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Copy one buffer in memory into another.
-
-        \fn static void CopyMemory( void *pvDst_, const void *pvSrc_, uint16_t u16Len_ )
-
-        \param pvDst_ Pointer to the destination buffer
-        \param pvSrc_ Pointer to the source buffer
-        \param u16Len_ Number of bytes to copy from source to destination
-    */
+     *  Copy one buffer in memory into another.
+     *
+     *  \fn static void CopyMemory( void *pvDst_, const void *pvSrc_, uint16_t u16Len_ )
+     *
+     *  \param pvDst_ Pointer to the destination buffer
+     *  \param pvSrc_ Pointer to the source buffer
+     *  \param u16Len_ Number of bytes to copy from source to destination
+     */
     static void CopyMemory( void *pvDst_, const void *pvSrc_, uint16_t u16Len_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Copy a string from one buffer into another.
-
-        \fn static void CopyString( char *szDst_, const char *szSrc_ )
-
-        \param szDst_ Pointer to the buffer to copy into
-        \param szSrc_ Pointer to the buffer to copy data from
-    */
+     *  Copy a string from one buffer into another.
+     *
+     *  \fn static void CopyString( char *szDst_, const char *szSrc_ )
+     *
+     *  \param szDst_ Pointer to the buffer to copy into
+     *  \param szSrc_ Pointer to the buffer to copy data from
+     */
     static void CopyString( char *szDst_, const char *szSrc_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Search for the presence of one string as a substring within another.
-
-        \fn static int16_t StringSearch( const char *szBuffer_, const char *szPattern_ )
-
-        \param szBuffer_ Buffer to search for pattern within
-        \param szPattern_ Pattern to search for in the buffer
-        \return Index of the first instance of the pattern in the buffer, or -1 on no match.
-    */
+     *  Search for the presence of one string as a substring within another.
+     *
+     *  \fn static int16_t StringSearch( const char *szBuffer_, const char *szPattern_ )
+     *
+     *  \param szBuffer_ Buffer to search for pattern within
+     *  \param szPattern_ Pattern to search for in the buffer
+     *  \return Index of the first instance of the pattern in the buffer, or -1 on no match.
+     */
     static int16_t StringSearch( const char *szBuffer_, const char *szPattern_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Compare the contents of two memory buffers to eachother
-
-        \fn static bool CompareMemory( void *pvMem1_, void *pvMem2_, uint16_t u16Len_ )
-
-        \param pvMem1_ First buffer to compare
-        \param pvMem2_ Second buffer to compare
-        \param u16Len_ Length of buffer (in bytes) to compare
-
-        \return true if the buffers match, false if they do not.
-    */
+     *  Compare the contents of two memory buffers to eachother
+     *
+     *  \fn static bool CompareMemory( void *pvMem1_, void *pvMem2_, uint16_t u16Len_ )
+     *
+     *  \param pvMem1_ First buffer to compare
+     *  \param pvMem2_ Second buffer to compare
+     *  \param u16Len_ Length of buffer (in bytes) to compare
+     *
+     *  \return true if the buffers match, false if they do not.
+     */
     static bool CompareMemory( const void *pvMem1_, const void *pvMem2_, uint16_t u16Len_ );
 
     //-----------------------------------------------------------------------
     /*!
-        Initialize a buffer of memory to a specified 8-bit pattern
-
-        \fn static void SetMemory( void *pvDst_, uint8_t u8Val_, uint16_t u16Len_ )
-
-        \param pvDst_ Destination buffer to set
-        \param u8Val_ 8-bit pattern to initialize each byte of destination with
-        \param u16Len_ Length of the buffer (in bytes) to initialize
-    */
+     *  Initialize a buffer of memory to a specified 8-bit pattern
+     *
+     *  \fn static void SetMemory( void *pvDst_, uint8_t u8Val_, uint16_t u16Len_ )
+     *
+     *  \param pvDst_ Destination buffer to set
+     *  \param u8Val_ 8-bit pattern to initialize each byte of destination with
+     *  \param u16Len_ Length of the buffer (in bytes) to initialize
+     */
     static void SetMemory( void *pvDst_, uint8_t u8Val_, uint16_t u16Len_ );
 
     //-----------------------------------------------------------------------
     /*!
-       \brief Tokenize Function to tokenize a string based on a space delimeter.  This is a
-              non-destructive function, which popu32ates a Token_t descriptor array.
-
-       \param szBuffer_ String to tokenize
-       \param pastTokens_ Pointer to the array of token descriptors
-       \param u8MaxTokens_ Maximum number of tokens to parse (i.e. size of pastTokens_)
-       \return Count of tokens parsed
-    */
+     * \brief Tokenize Function to tokenize a string based on a space delimeter.  This is a
+     *        non-destructive function, which popu32ates a Token_t descriptor array.
+     *
+     * \param szBuffer_ String to tokenize
+     * \param pastTokens_ Pointer to the array of token descriptors
+     * \param u8MaxTokens_ Maximum number of tokens to parse (i.e. size of pastTokens_)
+     * \return Count of tokens parsed
+     */
     static uint8_t Tokenize( const char *szBuffer_, Token_t *pastTokens_, uint8_t u8MaxTokens_);    
 };
 
