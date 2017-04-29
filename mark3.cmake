6aa4d2ead1bff5ad095c620764bdce1d170c242a
m0slevin <funk_dev@hotmail.com>
2017-04-28 22:42:03 -0400

    removing compiler checks

--- mark3.cmake
+++ mark3.cmake
@@ -26,9 +26,11 @@ get_property(mark3_objcopy_flags GLOBAL PROPERTY global_objcopy_flags)
 get_property(mark3_objcopy_dbg_flags GLOBAL PROPERTY global_objcopy_dbg_flags)
 
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR avr)
+set(CMAKE_SYSTEM_PROCESSOR ${mark3_arch})
 set(CMAKE_C_COMPILER ${mark3_cc})
 set(CMAKE_CXX_COMPILER ${mark3_cxx})
 set(CMAKE_CC_FLAGS "")
 set(CMAKE_CXX_FLAGS "")
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
 
